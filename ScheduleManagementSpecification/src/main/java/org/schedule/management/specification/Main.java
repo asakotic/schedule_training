41517334c5178ad0316b74c445169b65ff13c5a6
mehmedalijaK <rg.mehmedalija@gmail.com>
2023-10-29 18:36:52 +0100

    hello ana

--- ScheduleManagementSpecification/src/main/java/org/schedule/management/specification/Main.java
+++ ScheduleManagementSpecification/src/main/java/org/schedule/management/specification/Main.java
@@ -2,6 +2,6 @@ package org.schedule.management.specification;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello ana!");
     }
 }
