f51b6ea2ece94abdf538fc45aff723f74f50ce3c
asakotic6821rn <asakotic6821rn@raf.rs>
2023-11-11 20:18:18 +0100

    available appointments improvements

--- ScheduleManagementSpecification/src/main/java/org/schedule/management/specification/models/ScheduleSpecification.java
+++ ScheduleManagementSpecification/src/main/java/org/schedule/management/specification/models/ScheduleSpecification.java
@@ -25,21 +25,24 @@ public abstract class ScheduleSpecification {
     private MetaData metaData;
     private List<Appointment> appointments = new ArrayList<>();
     private List<String> headers;
-    public abstract void importDataCSV(String file,String config) throws IOException;
+
+    public abstract void importDataCSV(String file, String config) throws IOException;
+
     public abstract void importDataJSON() throws IOException; // uzme sobe, uzme praznike, meta podaci
+
     public abstract void exportDataPDF(String fileName);
 
     //da ima racunar, da ima vise od 10 racunara, da nema racunar
-    public List<Appointment> filterEquipment(List <Appointment> appointments, String equipment, int quantity){
+    public List<Appointment> filterEquipment(List<Appointment> appointments, String equipment, int quantity) {
         List<Appointment> group = new ArrayList<>();
 
-        for(Appointment a : appointments){
-            if(quantity == 0 && !(a.getRoom().getEquipment().containsKey(equipment))){
+        for (Appointment a : appointments) {
+            if (quantity == 0 && !(a.getRoom().getEquipment().containsKey(equipment))) {
                 group.add(a);
                 continue;
             }
-            if((a.getRoom().getEquipment().containsKey(equipment)) &&
-                    a.getRoom().getEquipment().get(equipment) >= quantity){
+            if ((a.getRoom().getEquipment().containsKey(equipment)) &&
+                    a.getRoom().getEquipment().get(equipment) >= quantity) {
                 group.add(a);
             }
         }
@@ -47,72 +50,75 @@ public abstract class ScheduleSpecification {
         return group;
     }
 
-    public List<Appointment> filterRelatedData(List<Appointment> appointments, String relatedDataKey, String relatedDataValue){
+    public List<Appointment> filterRelatedData(List<Appointment> appointments, String relatedDataKey, String relatedDataValue) {
         List<Appointment> group = new ArrayList<>();
 
-        for(Appointment a : appointments){
-            if(a.getRelatedData().containsKey(relatedDataKey) && a.getRelatedData().get(relatedDataKey).contains(relatedDataValue)){
+        for (Appointment a : appointments) {
+            if (a.getRelatedData().containsKey(relatedDataKey) && a.getRelatedData().get(relatedDataKey).contains(relatedDataValue)) {
                 group.add(a); //related data
             }
         }
 
         return group;
     }
-    public List<Appointment> filterDate(List<Appointment> appointments, LocalDate dateFrom, LocalDate dateTo){
+
+    public List<Appointment> filterDate(List<Appointment> appointments, LocalDate dateFrom, LocalDate dateTo) {
         List<Appointment> group = new ArrayList<>();
 
-        for(Appointment a : appointments){
-            if(!a.getDateFrom().toLocalDate().isBefore(dateFrom) && !a.getDateTo().toLocalDate().isAfter(dateTo))
-                group.add(a);
-        }
-
-        return group;
-    }
-    public List<Appointment> filterCapacity(List<Appointment> appointments, boolean greater, int capacity){
-        List<Appointment> group = new ArrayList<>();
-
-        for(Appointment a : appointments){
-            if(greater && capacity < Integer.parseInt(a.getRoom().getCapacity()))
-                group.add(a);
-            if(!greater && capacity > Integer.parseInt(a.getRoom().getCapacity()))
+        for (Appointment a : appointments) {
+            if (!a.getDateFrom().toLocalDate().isBefore(dateFrom) && !a.getDateTo().toLocalDate().isAfter(dateTo))
                 group.add(a);
         }
 
         return group;
     }
 
-    public List<Appointment> filterByRoom(List<Appointment> appointments, List<String> rooms){
+    public List<Appointment> filterCapacity(List<Appointment> appointments, boolean greater, int capacity) {
         List<Appointment> group = new ArrayList<>();
 
-        for(Appointment a : appointments){
-            if(rooms.contains(a.getRoom().getName()))
+        for (Appointment a : appointments) {
+            if (greater && capacity < Integer.parseInt(a.getRoom().getCapacity()))
+                group.add(a);
+            if (!greater && capacity > Integer.parseInt(a.getRoom().getCapacity()))
                 group.add(a);
         }
 
         return group;
     }
 
-    public List<Appointment> filterByReservedAppointments(){
+    public List<Appointment> filterByRoom(List<Appointment> appointments, List<String> rooms) {
+        List<Appointment> group = new ArrayList<>();
+
+        for (Appointment a : appointments) {
+            if (rooms.contains(a.getRoom().getName()))
+                group.add(a);
+        }
+
+        return group;
+    }
+
+    public List<Appointment> filterByReservedAppointments() {
         return appointments;
     }
 
-    public List<Appointment> resetFilter(){
+    public List<Appointment> resetFilter() {
         return appointments;
     }
 
-    public boolean checkAvailable(Appointment a, Appointment b){
+    public boolean checkAvailable(Appointment a, Appointment b) {
 
 
         return true;
     }
-    public List<Appointment> checkRelatedDataAvailable(List<Appointment> appointments, String relatedDataKey, String relatedDataValue){
+
+    public List<Appointment> checkRelatedDataAvailable(List<Appointment> appointments, String relatedDataKey, String relatedDataValue) {
         List<Appointment> filter = filterRelatedData(this.getAppointments(), relatedDataKey, relatedDataValue);
         List<Appointment> group = new ArrayList<>();
 
-        for(Appointment a : appointments){
+        for (Appointment a : appointments) {
 
-            for(Appointment f : filter){
-                if(!f.getDateFrom().isBefore(a.getDateFrom()) && !f.getDateFrom().isAfter(a.getDateTo())){ //ovo je kada se termin profesore nalazi u intervalu dva datuma
+            for (Appointment f : filter) {
+                if (!f.getDateFrom().isBefore(a.getDateFrom()) && !f.getDateFrom().isAfter(a.getDateTo())) { //ovo je kada se termin profesore nalazi u intervalu dva datuma
                     System.out.println();
                 }
             }
@@ -121,108 +127,115 @@ public abstract class ScheduleSpecification {
         }
 
 
-
         return group;
     }
 
-    public List<Appointment> searchByAvailableAppointments(){
+    public List<Appointment> searchByAvailableAppointments() {
         List<Appointment> group = new ArrayList<>();
         List<Room> rooms = metaData.getRooms();
-        LocalDate startDateTime = metaData.getScheduleValidFrom();
-        LocalDate endDateTime = metaData.getScheduleValidTo();
 
-        for(Room a : rooms){
-            while(!startDateTime.isAfter(endDateTime)){
-                LocalTime startWorkingHours = metaData.getWorkingHours().get(startDateTime.getDayOfWeek()).getOpeningTime();
+
+        for (Room a : rooms) {
+            LocalDate startDateTime = metaData.getScheduleValidFrom();
+            LocalDate endDateTime = metaData.getScheduleValidTo();
+            LocalTime tmp = metaData.getWorkingHours().get(startDateTime.getDayOfWeek()).getOpeningTime();
+
+            for (Appointment appointment : appointments) {
                 LocalTime endWorkingHours = metaData.getWorkingHours().get(startDateTime.getDayOfWeek()).getClosingTime();
 
+                if (!a.equals(appointment.getRoom())) continue;
 
-                for(Appointment appointment : appointments){
-                    if(!a.equals(appointment.getRoom()))continue;
-
-                    if(startDateTime.isBefore(appointment.getDateFrom().toLocalDate())){
-                        LocalDateTime ldt = LocalDateTime.of(startDateTime,startWorkingHours);
-                        group.add(new Appointment(a,ldt ,appointment.getDateTo()));
-                    }
-                    if(startDateTime.equals(appointment.getDateFrom().toLocalDate()) &&
-                            startWorkingHours.isBefore(appointment.getDateFrom().toLocalTime())){
-                        LocalDateTime ldt = LocalDateTime.of(startDateTime,startWorkingHours);
-                        group.add(new Appointment(a,ldt ,appointment.getDateTo()));
-                    }
-                    startWorkingHours =  appointment.getDateTo().toLocalTime();
-                    if(!startWorkingHours.isBefore(endWorkingHours))break;
-
+                if (startDateTime.isBefore(appointment.getDateFrom().toLocalDate())) {
+                    LocalDateTime ldt = LocalDateTime.of(startDateTime, tmp);
+                    Appointment bla = new Appointment(a, ldt, appointment.getDateFrom());
+                    group.add(bla);
+                    System.out.println("ispis1" + bla);
+                    startDateTime = appointment.getDateTo().toLocalDate();
+                } else if (startDateTime.equals(appointment.getDateFrom().toLocalDate()) &&
+                        tmp.isBefore(appointment.getDateFrom().toLocalTime())) {
+                    LocalDateTime ldt = LocalDateTime.of(startDateTime, tmp);
+                    Appointment bla = new Appointment(a, ldt, appointment.getDateFrom());
+                    group.add(bla);
+                    System.out.println("ispis2" + bla);
                 }
-                startDateTime = startDateTime.plusDays(1);
+                tmp = appointment.getDateTo().toLocalTime();
+                if (!tmp.isBefore(endWorkingHours)){
+                    startDateTime = startDateTime.plusDays(1);
+                    tmp = metaData.getWorkingHours().get(startDateTime.getDayOfWeek()).getOpeningTime();
+                }
             }
+            if(tmp.isBefore(metaData.getWorkingHours().get(endDateTime.getDayOfWeek()).getClosingTime()) ||
+                startDateTime.isBefore(endDateTime)){
+                group.add(new Appointment(a, startDateTime.atTime(tmp),
+                        endDateTime.atTime(metaData.getWorkingHours().get(endDateTime.getDayOfWeek()).getClosingTime())));
+            }
+
         }
-
-
-
-
-
         return group;
     }
 
-    public  void search(){
-
-
+    public void search() {
 
 
     }
-    public void importMeta(){
+
+    public void importMeta() {
         metaData = MetaData.importMeta();
         System.out.println(metaData);
     }
-    public boolean addRoom(String roomName, String capacity, Map<String, Integer> equipment){
+
+    public boolean addRoom(String roomName, String capacity, Map<String, Integer> equipment) {
         Room r = new Room(roomName, capacity, equipment);
-        if(!metaData.getRooms().contains(r)){
+        if (!metaData.getRooms().contains(r)) {
             metaData.getRooms().add(r);
             return true;
         }
         //TODO: Exception
         return false;
     }
-    public  boolean addAppointment(Appointment appointment){
+
+    public boolean addAppointment(Appointment appointment) {
         LocalDateTime start = appointment.getDateFrom();
         LocalDateTime end = appointment.getDateTo();
-        if (start.isAfter(end))return false;
-        for(Appointment a : this.getAppointments()){
-            if(!a.getRoom().getName().equals(appointment.getRoom().getName())) continue;
-            LocalDateTime aStart= a.getDateFrom();
-            LocalDateTime aEnd= a.getDateFrom();
-            if(start.isAfter(aStart) && start.isBefore(aEnd) ||
-                end.isBefore(aEnd) && end.isAfter(aStart) ||
-                start.isBefore(aStart) && end.isAfter(aEnd) ||
-                start.isAfter(aStart) && end.isBefore(aEnd)) {
+        if (start.isAfter(end)) return false;
+        for (Appointment a : this.getAppointments()) {
+            if (!a.getRoom().getName().equals(appointment.getRoom().getName())) continue;
+            LocalDateTime aStart = a.getDateFrom();
+            LocalDateTime aEnd = a.getDateFrom();
+            if (start.isAfter(aStart) && start.isBefore(aEnd) ||
+                    end.isBefore(aEnd) && end.isAfter(aStart) ||
+                    start.isBefore(aStart) && end.isAfter(aEnd) ||
+                    start.isAfter(aStart) && end.isBefore(aEnd)) {
                 return false;
             }
         }
         this.appointments.add(appointment);
         return true;
     }
-    public  boolean deleteAppointment(LocalDateTime from, LocalDateTime to, String roomName){
+
+    public boolean deleteAppointment(LocalDateTime from, LocalDateTime to, String roomName) {
         boolean flag = false;
         Appointment removeA = null;
-        for(Appointment a: this.appointments){
-            if(a.getDateFrom().equals(from) &&
-               a.getDateTo().equals(to) &&
-               a.getRoom().getName().equals(roomName)){
+        for (Appointment a : this.appointments) {
+            if (a.getDateFrom().equals(from) &&
+                    a.getDateTo().equals(to) &&
+                    a.getRoom().getName().equals(roomName)) {
                 flag = true;
                 removeA = a;
                 break;
             }
         }
-        if(flag)appointments.remove(removeA);
+        if (flag) appointments.remove(removeA);
         return flag;
     }
-    public  boolean rescheduleAppointment(LocalDateTime from, LocalDateTime to,String room, Appointment appointment){
-       return deleteAppointment(from,to,room)
-               && addAppointment(appointment);
+
+    public boolean rescheduleAppointment(LocalDateTime from, LocalDateTime to, String room, Appointment appointment) {
+        return deleteAppointment(from, to, room)
+                && addAppointment(appointment);
     }
 
 
-    protected List<ConfigMapping> importConfig(String configPath){
+    protected List<ConfigMapping> importConfig(String configPath) {
         List<ConfigMapping> map = new ArrayList<>();
         int br = 0;
         File file;
@@ -235,19 +248,20 @@ public abstract class ScheduleSpecification {
                 String[] split = line.split(" ", 3);
                 ConfigMapping cm = new ConfigMapping(Integer.parseInt(split[0]), split[1], split[2]);
                 map.add(cm);
-                if(br++ != cm.getIndex()) return null; //TODO BACI EKSEPSNNNNN
+                if (br++ != cm.getIndex()) return null; //TODO BACI EKSEPSNNNNN
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (sc != null) {
                 sc.close();
             }
         }
         return map;
     }
-    public void exportDataCSV(String fileName, String configpath, List<Appointment> appointments){
+
+    public void exportDataCSV(String fileName, String configpath, List<Appointment> appointments) {
 
         List<ConfigMapping> configMap = importConfig(configpath);
         configMap.sort(Comparator.comparingInt(ConfigMapping::getIndex));
@@ -277,7 +291,7 @@ public abstract class ScheduleSpecification {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 csvPrinter.close();
                 fileWriter.close();
@@ -286,9 +300,10 @@ public abstract class ScheduleSpecification {
             }
         }
     }
-    public void exportDataJSON(List<Appointment> appointments, String fileName){
+
+    public void exportDataJSON(List<Appointment> appointments, String fileName) {
         Gson gson = new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class,new LocalDateTimeAdapter())
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
                 .setPrettyPrinting()
                 .create();
         try (PrintStream writer = new PrintStream(fileName)) {
